a3abec2758625bdd907dda67b21784e9a52d09de
chen <57761823+chenTest2019@users.noreply.github.com>
2020-10-18 14:23:38 +0800

    jdk study

--- java/src/main/java/SetTest.java
+++ java/src/main/java/SetTest.java
@@ -1,18 +1,33 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SetTest {
     public static void main(String[] args) {
 
         HashMap<String,String> hashMap = new HashMap<>();
 
+        hashMap.put("a","1");
+        hashMap.put("b","2");
+
+
         TreeMap<String, String> treeMap = new TreeMap<>();
+        treeMap.put("a","1");
+        treeMap.put("b","2");
+
+        System.out.println(hashMap);
+        System.out.println(treeMap);
 
         HashSet<String> hashSet = new HashSet<>();
+        hashSet.add("a");
+        hashSet.add("b");
 
         TreeSet<String> treeSet = new TreeSet<>();
+        treeSet.add("a");
+        treeSet.add("b");
+
+
+
+        Hashtable<String,String> hashtable=new Hashtable<>();
+
 
 
     }
